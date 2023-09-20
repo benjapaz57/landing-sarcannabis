1d134bc43b84efc78cbd37d96058afbec2ef26ec
dev <benjapaz57@gmail.com>
2023-09-20 15:53:56 -0300

    asd

---
+++ src/app/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    // Inicializa el carousel de Bootstrap
+    $('#colaboradores').carousel();
+
+    // Configura la velocidad de desplazamiento y dirección
+    $('#colaboradores').carousel({
+        interval: 300, // Cambia cada 3 segundos
+        pause: 'hover' // Pausa en hover del mouse
+    });
+});
